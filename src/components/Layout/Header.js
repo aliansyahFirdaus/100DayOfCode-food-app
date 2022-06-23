37475236ff36abee80d84150e276c3829e8f2c0e
aliansyahFirdaus <aliansyah.it@gmail.com>
2022-06-23 22:03:10 +0700

    add folder structures

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+export default function Header() {
+  return (
+    <Fragment>
+      <header>
+        <h1></h1>
+        <button>cart</button>
+      </header>
+      <div>
+        <img />
+      </div>
+    </Fragment>
+  )
+}
