df3e1b703a0f1362c12b88ff40ca74a8fde8b857
aliansyahFirdaus <aliansyah.it@gmail.com>
2022-06-23 22:08:38 +0700

    add assets img hero banner, add styling for header/navigation, delete h1 on app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from "./components/Layout/Header";
 function App() {
   return (
     <Fragment>
-      <h2>Let's get started!</h2>
       <Header />
     </Fragment>
   );
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,17 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
+import styles from "./Header.module.css";
+import heroImg from "../../assets/meals.jpg";
 
 export default function Header() {
   return (
     <Fragment>
-      <header>
-        <h1></h1>
+      <header className={styles.header}>
+        <h1>Header</h1>
         <button>cart</button>
       </header>
-      <div>
-        <img />
+      <div className={styles["main-image"]}>
+        <img src={heroImg} />
       </div>
     </Fragment>
-  )
+  );
 }
